5d99fe5a5699b52dc7ad19765d3eb13199d56533
ramimhossain12 <ramimhossain6242@gmail.com>
2020-08-19 20:50:18 +0600

    first commit

---
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnID);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(in);
+            }
+        });
+    }
+}
